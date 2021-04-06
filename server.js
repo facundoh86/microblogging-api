3530aa1f6e0b935b8292aa77f49d09cdd7511dee
fherrera <facundoh1986@gmail.com>
2021-04-06 13:02:12 +0200

    Second commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const app    = require('./app');
+const server = require('http').Server(app);
+const debug  = require('debug')('app:server');
+const port   = process.env.NODE_PORT || 3000;
+
+server.listen(port, () => {
+
+    debug(`Servidor corriendo en el puerto: ${server.address().port}`);
+});
